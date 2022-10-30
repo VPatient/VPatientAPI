2fc2cba4e686c52f6a108ba0bd45b5cf948ce23a
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2022-10-30 22:00:17 +0300

    project infr improved and MongoDb conn established

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");  // import express
 const app = express(); // express app
 const dotenv = require('dotenv'); // import dotenv
 const mongoose = require("mongoose") // import MongoDb
+const cors = require('cors'); // for CORS policy
+const bodyParser = require('body-parser'); // body parser to parse as json
 dotenv.config(); // configure dotenv to use secret keys
 
 
@@ -11,14 +13,16 @@ const exampleRoute = require('./routes/exampleRoute');
 
 
 // connect mongodb server- will be implemented later
-// mongoose.connect(
-//     process.env.MONGO_DB_URL
-// )
-// .then(()=>console.log("DB Connection Successfull"))
-// .catch((err)=>console.log(err));
+mongoose.connect(
+    process.env.MONGO_DB_URL
+)
+.then(()=>console.log("DB Connection Successfull"))
+.catch((err)=>console.log(err));
 
 //middlewares
 app.use(express.json());
+app.use(bodyParser.json());// using body-parser for the requests
+app.use(cors()); // for CORS-POLICY
 
 app.use("/api/exampleRoute",exampleRoute); // use exampleRoute endpoints if url starts with /api/exampleRoute
 
--- routes/exampleRoute.js
+++ routes/exampleRoute.js
@@ -10,26 +10,35 @@ router.get("/exampleGet",(req,res)=>{
 router.post("/examplePost",async (req,res)=>{
 
     //save a record for example
-    // const ex = new ExampleModel({
-    //     title: req.body.title,
-    //     description: req.body.description,
-    //     rating: req.body.example
-    // });
-    // ex.save()
-    // .then(ex=>res.json(ex))
-    // .catch(err=>res.json({message:err}));
-
-    res.send("Example Post is Successfull!");
+    const ex = new ExampleModel({
+        title: req.body.title,
+        description: req.body.description,
+        ex: req.body.ex
+    });
+    ex.save()
+    .then(ex=>res.json(ex))
+    .catch(err=>res.json({message:err}));
 });
 
 // example put with params
 router.put("/:id", async (req,res)=>{
-    res.send(`Example Put Successfull for id: ${req.params.id}`);
+    try{
+        const updatedEntry = await ExampleModel.findByIdAndUpdate(req.params.id,{
+            $set: req.body
+        }, {new:true});
+
+        res.status(200).json(updatedEntry);
+        
+    }catch(err){
+        res.status(500).json(err);
+    }
 });
 
 // example delete with params
 router.delete("/:id",async (req,res)=>{
-    res.send(`Example Delete is Successfull for id: ${req.params.id}`);
+    ExampleModel.findByIdAndRemove(req.params.id)
+    .then(entry=>res.json(`Successfully deleted: ${entry.title}`))
+    .catch(err=>res.json({message:err}))
 });
 
 
