20f6a2ba5934095313f85f20516eda168d4fea5e
Tzesh <mail@ugurdindar.com>
2022-11-04 02:04:11 +0300

    register route response changed

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/UserModel');
-const {registerValidation,loginValidation} = require('../../common/validation')
+const { registerValidation, loginValidation } = require('../../common/validation')
 const jwt = require('jsonwebtoken');
 
-router.post('/register', async (req,res)=>{
+router.post('/register', async (req, res) => {
 
     //validation
-    const {error} = registerValidation(req.body);
-    if(error) return res.status(400)
-                        .send(error.details[0].message);
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400)
+        .send(error.details[0].message);
 
     //checking if email is already taken
-    User.findOne({email: req.body.email})
-        .then(emailExists=>{
-            if(emailExists) return res.status(400).send('Email already exists.')
+    User.findOne({ email: req.body.email })
+        .then(emailExists => {
+            if (emailExists) return res.status(400).send('Email already exists.')
         })
 
     //hash the password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password,salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 
     const user = new User({
@@ -29,38 +29,38 @@ router.post('/register', async (req,res)=>{
         password: hashedPassword
     })
     user.save()
-    .then(savedUser=>res.json({user: savedUser._id}))
-    .catch(err=>res.json({"messages":err}));
+        .then(savedUser => res.send({ user: savedUser._id }))
+        .catch(err => res.send({ "messages": err }));
 });
 
 
-router.post('/login',async (req,res)=>{
+router.post('/login', async (req, res) => {
     //validation
-    const {error} = loginValidation(req.body);
-    if(error) return res.status(400)
-                        .send(error.details[0].message);
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400)
+        .send(error.details[0].message);
 
     //checking if email is on the database
-    const user = await User.findOne({email: req.body.email});
-    if(!user) return res.status(400).send('Wrong e-mail or password.');
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Wrong e-mail or password.');
 
     //is password correct?
     const validPassword = await bcrypt
-                    .compare(req.body.password,
-                            user.password)
-    if(!validPassword) return res.status(400).send('Wrong e-mail or password.');
+        .compare(req.body.password,
+            user.password)
+    if (!validPassword) return res.status(400).send('Wrong e-mail or password.');
 
     //create and assign jwt
     const token = jwt.sign(
         {
-            _id:user._id,
-            name:user.name,
+            _id: user._id,
+            name: user.name,
             studentNumber: user.studentNumber,
             email: user.email
 
-        },process.env.SECRET);
+        }, process.env.SECRET);
 
-    res.header('auth-token',token).send(token);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router
