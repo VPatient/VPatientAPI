ffe9400d325aa84b0104058a3c2752506f8848cc
Tzesh <mail@ugurdindar.com>
2022-12-11 23:27:59 +0300

    changed sorting desc -> asc

--- routes/patient/bloodsugar/bloodsugar.js
+++ routes/patient/bloodsugar/bloodsugar.js
@@ -40,7 +40,7 @@ router.get("/get", auth, verifyPatient, async (req, res) => {
     let patient = req.patient;
 
     // get results sorted to the time that dosage is taken
-    const traces = await BloodSugarTraceModel.find({ owner: patient }).sort({ time: -1 });
+    const traces = await BloodSugarTraceModel.find({ owner: patient }).sort({ time: 1 });
 
     // return
     res.status(200).json(traces);
